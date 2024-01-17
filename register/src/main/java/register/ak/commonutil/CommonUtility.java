2589985ee9905f1d89ba57ac2c1667db3dd4dd14
Asit patel <githubbbsr007@gmail.com>
2024-01-17 12:18:09 +0530

    added jdbc db details

---
+++ register/src/main/java/register/ak/commonutil/CommonUtility.java
@@ -0,0 +1,30 @@
+package register.ak.commonutil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CommonUtility {
+	public static void close(Connection con, PreparedStatement stmt, ResultSet result) {
+		try {
+			if (result != null) {
+				result.close();
+			}
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (con != null) {
+				con.close();
+			}
+
+		} catch (SQLException sqlEx) {
+			System.out.println(sqlEx);
+		}
+	}
+	
+	public static Connection getJdbcConnection() throws SQLException {
+		return DriverManager.getConnection("jdbc:mysql://localhost:3306/piresqure", "root", "root");		
+	}
+}
--- register/src/main/java/register/ak/controller/RegisterServlet.java
+++ register/src/main/java/register/ak/controller/RegisterServlet.java
@@ -2,14 +2,17 @@ package register.ak.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import register.ak.commonutil.CommonUtility;
 
 @WebServlet("/registerUser")
 public class RegisterServlet extends HttpServlet {
@@ -18,25 +21,40 @@ public class RegisterServlet extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 668021950483083829L;
-	Map<String,String> listUser;
 
 	@Override
 	public void init() throws ServletException {
 		System.out.println("initialization for store ing user object");
-		listUser = new HashMap<>();
 	}
-	
+
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {		
-		String userName=req.getParameter("username");
-		String password=req.getParameter("password");
-		String emailId=req.getParameter("emailId");
-		System.out.println(emailId);
-		System.out.println(userName);
-		listUser.put(userName, password);		
-		PrintWriter writer=resp.getWriter();
-		writer.print("<h1>user register success with email id "+ emailId +"</h1>");
-		System.out.println(listUser.size());
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String userName = req.getParameter("username");
+		String password = req.getParameter("password");
+		String emailId = req.getParameter("emailId");
+		Connection con = null;
+		String sql = "insert into students  (username,password,email) value(?,?,?)";
+		PreparedStatement stmt = null;
+		// connection
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = CommonUtility.getJdbcConnection();
+			stmt = con.prepareStatement(sql);
+			stmt.setString(1, userName);
+			stmt.setString(2, password);
+			stmt.setString(3, emailId);
+			stmt.execute();
+			System.out.println("data stored successfully");
+		} catch (SQLException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			CommonUtility.close(con, stmt, null);
+		}
+
+		PrintWriter writer = resp.getWriter();
+		writer.print("<h1>user register success with email id " + emailId + "</h1>");
+
 	}
 
 }
